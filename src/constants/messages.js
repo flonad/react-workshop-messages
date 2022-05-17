feaa8052ecb3897a48e4376d3032bfe2dd247362
Florian Nadaud <florian.nadaud@serli.com>
2022-05-17 16:22:18 +0200

    fix message list

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,6 +1,6 @@
-export const messages = [
-  { id: 1, author: "Sébastien", text: "Hello World" },
-  { id: 2, author: "Mathieu", text: "I Love React" },
-  { id: 3, author: "Guillaume", text: "Javascript Forever" },
-  { id: 4, author: "Fedy", text: "I Love React, but running a marathon is more awesome" }
+export const MESSAGE_LIST = [
+  { author: "Sébastien", text: "Hello World" },
+  { author: "Mathieu", text: "I Love React" },
+  { author: "Guillaume", text: "Javascript Forever" },
+  { author: "Fedy", text: "I Love React, but running a marathon is more awesome" }
 ];
